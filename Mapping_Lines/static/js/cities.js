68456694691e664c76f711831fab72c185ea8d2a
Eric Langendorff <eric.langendorff@asiafoundation.org>
2022-06-29 14:01:45 -0700

    Path Between Airports

---
+++ Mapping_Lines/static/js/cities.js
@@ -0,0 +1,43 @@
+let usgc = [40.7, -94.5];   // United States (approximate) geographic center
+let cities = [
+    {
+        location: [40.7128, -74.0059],
+        city: "New York City",
+        state: "NY",
+        population: 8398748
+    },
+    {
+        location: [41.8781, -87.6298],
+        city: "Chicago",
+        state: "IL",
+        population: 2705994
+    },
+    {
+        location: [29.7604, -95.3698],
+        city: "Houston",
+        state: "TX",
+        population: 2325502
+    },
+    {
+        location: [34.0522, -118.2437],
+        city: "Los Angeles",
+        state: "CA",
+        population: 3990456
+    },
+    {
+        location: [33.4484, -112.0740],
+        city: "Phoenix",
+        state: "AZ",
+        population: 1660272
+    }
+];
+let airports = {
+        "AUS": [30.1975, -97.6664],
+        "JFK": [40.6413, -73.7781],
+        "LAX": [33.9416, -118.4085],
+        "SFO": [37.6213, -122.3790],
+        "SLC": [40.7899, -111.9791],
+        "SEA": [47.4502, -122.3088],
+        "YYZ": [43.6777, -79.6248]
+};
+
---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,78 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+        center: usgc,
+        zoom: 5
+    });
+
+// Create tile layer for map background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        // Mapbox styles. See https://docs.mapbox.com/api/maps/styles/
+        // id: 'mapbox/streets-v11',
+        // id: 'mapbox/outdoors-v11',
+        id: 'mapbox/light-v10',
+        // id: 'mapbox/dark-v10',
+        // id: 'mapbox/satellite-v9',
+        // id: 'mapbox/satellite-streets-v11',
+        // id: 'mapbox/navigation-day-v1',
+        // id: 'mapbox/navigation-night-v1',
+        // tileSize: 512,
+        // zoomOffset: -1,
+        accessToken: API_KEY
+    });
+
+// Add 'graymap' tile layer to map.
+streets.addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Coordinates for each point to be used in the line.
+let route = [
+    airports.SFO,
+    airports.AUS,
+    airports.YYZ,
+    airports.JFK
+];
+
+// Create a polygonal line using the line coordinates and make the line red.
+L.polyline(route, {
+    color: "blue",
+    opacity: 0.5,
+    dashArray: "5,7"
+}).addTo(map);
+
+//  Add marker to map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);     // Basic pin-style marker
+// let marker = L.circle([34.0522, -118.2437], {    // radius for `circle()` is in meters
+//         // radius: 100,
+//         radius: 300,
+//         color: "black",
+//         fillColor: "lightyellow",
+//     }).addTo(map);
+
+// let marker = L.circleMarker([34.0522, -118.2437], { // radius for `circleMarker` is in pixels
+//         radius: 300,
+//         color: "black",
+//         // fillColor: "lightyellow",
+//         fillColor: "#FFFFA1",
+//     }).addTo(map);
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//         console.log(city);
+//         // L.marker(city.location)
+//         L.circleMarker(city.location, { // radius for `circleMarker` is in pixels
+//             radius: city.population / 200000,
+//             color: "orange",
+//             weight: 4,
+//             // dashArray: "5,7"  // gap-length, dash-length
+//         })
+//         .bindPopup(`<h2>${city.city}, ${city.state}</h2> <hr> <h3>Population ${city.population.toLocaleString()}</h3>`)
+//         .addTo(map);
+//     });
+
